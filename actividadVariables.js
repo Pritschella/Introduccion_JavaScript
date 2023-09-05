06ef81a55f62634e8ddb13cfedda79dc00eeedf5
Pritschella <prits99@hotmail.com>
2023-09-04 18:27:26 -0600

    Se agregan los archivos creados en clase acerca de variables y tipos de datos

---
+++ actividadVariables.js
@@ -0,0 +1,32 @@
+//String
+var nombrePaciente = "Berenice";
+var apellidoPaciente= "Flores";
+var telefono = "4949499994";
+
+//Number
+var edad = 24;
+var costoLimpieza = 400;
+
+//Boolean
+var pacienteRegistrado = true;
+var tratamientoPrevio = false;
+
+//Undefined
+var diaDisponible;
+var costoTotal;
+
+//Null
+var genero = null;
+var seguroMedico = null;
+
+
+// --- Consversión cadenas a números ----
+var telefonoNumero = parseInt(telefono);
+console.log(telefonoNumero);
+console.log(typeof(telefonoNumero));
+
+//--- Converesion números a cadenas ---
+var edadCadena = edad.toString();
+console.log(edadCadena);
+console.log(typeof(edadCadena));
+
---
+++ script.js
@@ -0,0 +1,72 @@
+//alert("Hola mundo");
+
+/* 
+este
+es
+un
+comentario
+multilínea
+*/
+
+/* var frasco = "pastillas";
+var frasco = "pasitas con chocolate";
+var frasco = "shampoo";
+
+console.log(frasco); */
+
+/*Tipos de datos primitivos y no primitivos
+
+string
+number
+boolean
+undefined
+null
+symbol
+object(no es primitivo)
+*/
+
+//string es una cadena de texto, que siempre debe ir entre comillas simles o comillas dobles.
+// var vaso ="leche";
+// var vaso = "3825871030";
+
+//number no lleva comillas, y se refiere a un número con el cual podemos hacr operaciones.
+// var edad = 31;
+
+//boolean solamente tiene dos salidas que es true or false. 
+// var cuentasPremium = false;
+
+//undefined es un tipo de ato que existe pero que no está definido o inicializado.
+// var proximaCita = ;
+
+
+
+//null es un valor que no tenemos pero que declaramos
+//var asistenciaInvitado = null;
+
+var edad = "31";
+var peso = 66;
+
+//console.log(typeof(edad));
+/* typeof es una palabra reservada que nos ayuda a saber qué tio de dato tenemos. */
+
+
+//var es una variable con alcance global (tiene mayor alcance). 
+
+//let es una variable con alcance local (solo se utliza dentro de un bloque de código).
+
+//let es una variable con alcance local (solo se utiliza dentro de un bloque de código).
+
+
+//Convertir un número a cadena
+var pesoCadena  = peso.toString();
+console.log(pesoCadena);
+console.log(typeof(pesoCadena));
+
+//Convertir una cadena a un número
+var edadNumero = parseInt(edad);
+console.log(edadNumero);
+console.log(typeof(edadNumero));
+
+
+
+
